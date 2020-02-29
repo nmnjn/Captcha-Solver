880fff23ff7604eabb48cb26dc6accaa236cd2ca
Naman Jain <naman17@gmail.com>
2020-02-29 07:10:19 +0000

    Added Basic Logs with Flask Profiler

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
+from flask import Flask, jsonify, request
 from captchaSolver import getCaptcha
+import flask_profiler
 
-cont = 'y'
-while cont == 'y':
-	print("Enter the Image URL: ")
-	url = input()
-	print(getCaptcha(url))
-	print("continue? (y/n)")
-	cont = input()
+app = Flask(__name__)
+
+app.config["flask_profiler"] = {
+        "enabled": True,
+        "storage": {
+            "engine": "sqlite"
+         },
+        "basicAuth":{
+            "enabled": True,
+            "username": "admin",
+            "password": "password"
+        }
+     }
+
+
+
+@app.route('/solve', methods=['GET'])
+def solve():
+	args = request.args
+	if "url" in args:
+		url = args["url"]
+		print(url)
+		resp = getCaptcha(url)
+		return jsonify(resp)
+	return "Please append a valid URL [?url=]"
+
+
+flask_profiler.init_app(app)
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port='5000', threaded=True)
--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 from flask import Flask, jsonify, request
 from captchaSolver import getCaptcha
+import flask_profiler
 
 app = Flask(__name__)
+
+app.config["flask_profiler"] = {
+        "enabled": True,
+        "storage": {
+            "engine": "sqlite"
+         },
+        "basicAuth":{
+            "enabled": True,
+            "username": "admin",
+            "password": "password"
+        }
+     }
+
+
 
 @app.route('/solve', methods=['GET'])
 def solve():
@@ -13,5 +28,9 @@ def solve():
 		return jsonify(resp)
 	return "Please append a valid URL [?url=]"
 
+
+flask_profiler.init_app(app)
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='9999', threaded=True)
+	app.run(host='0.0.0.0', port='5000', threaded=True)
